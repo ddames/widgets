38181c4a5c5553fc38f4086219a64a97a605ca58
ddames <daviddames@gmail.com>
2021-02-19 13:13:49 -0600

    Adding useState to make dropdown show and hide contents.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
+import Dropdown from './components/Dropdown';
 import Search from './components/Search';
 
 const items = [
@@ -17,12 +18,31 @@ const items = [
   }
 ]
 
-const App = () => {
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  },
+  {
+    label: 'Shade of Blue',
+    value: 'blue'
+  }
+]
+
+export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Search />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
-  )
+  );
 };
-
-export default App;
