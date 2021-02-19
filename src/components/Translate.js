083d17e370089f14fb0b66da6038a2b48c479dcf
ddames <daviddames@gmail.com>
2021-02-19 16:51:04 -0600

    Erasing API key history. Adding some formatting to the input field for translate.

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -18,9 +18,15 @@ const options = [
 
 const Translate = () => {
   const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState('');
 
   return (
     <div>
+      <div className="ui form">
+        <div className="field">
+          <input value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+      </div>
       <Dropdown
         label="Select a Language"
         selected={language}
